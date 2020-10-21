d93bd5f0eac11f4d9154c2f84c439df1835d4db0
Luiz Fernando <luizfernandorole@gmail.com>
2020-10-21 10:55:27 -0300

    Alterando URL de deploy, implementação de template para o Jefferson fazer a descrição do site

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,16 +4,18 @@ import {CardModule} from '../card/card.module';
 import { HeaderComponent } from './header/header.component';
 import {RouterModule} from '@angular/router';
 import { CardsUserComponent } from './cards-user/cards-user.component';
+import {ProjectDescriptionComponent} from './project-description/project-description.component';
+import {LoginComponent} from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [ HeaderComponent, CardsUserComponent ],
+  declarations: [ HeaderComponent, CardsUserComponent, ProjectDescriptionComponent, LoginComponent ],
   exports: [ HeaderComponent ],
   imports: [
     CommonModule,
     CardModule,
-    RouterModule,
+    RouterModule
   ]
 })
 export class HomeModule { }
